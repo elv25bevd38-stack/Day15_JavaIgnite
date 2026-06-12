6473d8536510f9e09fdbfd09ec21efb08368f671
Ayush Samanta <elv.25bevd38@silicon.ac.in>
2026-06-12 10:14:32 +0530

    Implement classroom attendance system with threads

--- Multithreading/Classroom_attendance_system.java
+++ Multithreading/Classroom_attendance_system.java
@@ -89,3 +89,80 @@ Main Method
 Thread Coordination
 Synchronization
 */
+class Classroom {
+    private boolean attendanceStarted = false;
+
+    public synchronized void waitForAttendance() {
+        while (!attendanceStarted) {
+            try {
+                System.out.println(Thread.currentThread().getName()
+                        + " : Student Waiting...");
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(Thread.currentThread().getName()
+                + " : Student Marked Present");
+    }
+
+    public synchronized void startAttendance() {
+        attendanceStarted = true;
+
+        System.out.println("Teacher Started Attendance");
+
+        notifyAll();
+    }
+}
+
+class TeacherThread extends Thread {
+    Classroom classroom;
+
+    TeacherThread(Classroom classroom) {
+        this.classroom = classroom;
+    }
+
+    public void run() {
+        classroom.startAttendance();
+    }
+}
+
+class StudentThread extends Thread {
+    Classroom classroom;
+
+    StudentThread(Classroom classroom, String name) {
+        this.classroom = classroom;
+        setName(name);
+    }
+
+    public void run() {
+        classroom.waitForAttendance();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Classroom classroom = new Classroom();
+
+        StudentThread s1 = new StudentThread(classroom, "Student-1");
+        StudentThread s2 = new StudentThread(classroom, "Student-2");
+        StudentThread s3 = new StudentThread(classroom, "Student-3");
+
+        TeacherThread teacher = new TeacherThread(classroom);
+
+        // Start students first
+        s1.start();
+        s2.start();
+        s3.start();
+
+        try {
+            Thread.sleep(3000); // Teacher comes after 3 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        teacher.start();
+    }
+}
