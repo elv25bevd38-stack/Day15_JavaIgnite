343d147cb90584ea6c4dff8fde3779eab5fba2c7
Ayush Samanta <elv.25bevd38@silicon.ac.in>
2026-06-12 10:19:04 +0530

    Implement synchronized seat booking in Theater class

--- Multithreading/TicketBookingSystem.java
+++ Multithreading/TicketBookingSystem.java
@@ -79,3 +79,57 @@ Possible Output
 
     Thread3 Booking Failed
 */
+class Theater {
+
+    private int totalSeats = 5;
+
+    public synchronized void bookSeat(int seats) {
+
+        if (totalSeats >= seats) {
+
+            System.out.println(Thread.currentThread().getName()
+                    + " Booking Successful");
+
+            totalSeats = totalSeats - seats;
+
+            System.out.println("Remaining Seats = " + totalSeats);
+        } else {
+
+            System.out.println(Thread.currentThread().getName()
+                    + " Booking Failed");
+        }
+    }
+}
+
+class BookingThread extends Thread {
+
+    private Theater theater;
+    private int seatsRequired;
+
+    public BookingThread(Theater theater, int seatsRequired, String name) {
+        this.theater = theater;
+        this.seatsRequired = seatsRequired;
+        setName(name);
+    }
+
+    @Override
+    public void run() {
+        theater.bookSeat(seatsRequired);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Theater theater = new Theater();
+
+        BookingThread t1 = new BookingThread(theater, 2, "Thread1");
+        BookingThread t2 = new BookingThread(theater, 2, "Thread2");
+        BookingThread t3 = new BookingThread(theater, 2, "Thread3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
